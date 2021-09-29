a5b7c61c7f80e831f2d09665549ccf0dadb5e645
3840x2400 <88857154+3840x2400@users.noreply.github.com>
2021-09-30 00:22:02 +1000

    Delete firstexe6.py

--- firstexe6.py
+++
@@ -1,7 +0,0 @@
-distance = float(input("Дистанция в первый день: "))
-goal = float(input("Цель   "))
-days = 1
-while distance < goal:
-    distance *= 1.1
-    days += 1
-print(f'Требуемое количество - {days}')
